42e4ef7ebfc78fd1d886f2fc24476ae2d81125c7
precioustosin <precioustosin@hotmail.com>
2016-08-17 14:15:41 +0100

    Defining Methods

--- NoteApplication.js
+++ NoteApplication.js
@@ -4,4 +4,20 @@ class NotesApplication{
 	 this.author = author;
 	 this.noteArray = [];
 	 this.noteArray = new Author();
+	 
+	create(note_content){
+		this.noteArray.push(note_content);
+ }
+	listNotes(){
+		for(i = 0; i < this.noteArray; i++ ){
+			return "NoteID:"+" "+ i + \n + noteArray[i] + \n + "By Author" + " " + this.author;  
+		}
+	}
+	get(node_id){
+		for(i > 0; i < noteArray.Lenght; i++){
+			if(noteArray[i] === noteArray[note_id]){
+				return noteArray[note_id];
+			}
+		}
+	}
 }
